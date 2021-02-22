8f8a71cfbb0c188d279e05c9a3c6e4535065b6e8
TSubbota <tsubbota@nativeroll.tv>
2021-02-22 16:44:43 +0300

    Build password generator form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  password = '';
+  options = {
+    useLetters: false,
+    useNumbers: false,
+    useSymbols: false,
+    length: 0,
+  };
+  onChangeUse(value: string): void {
+    switch (value) {
+      case 'letters':
+        this.options.useLetters = !this.options.useLetters;
+        break;
+      case 'numbers':
+        this.options.useNumbers = !this.options.useNumbers;
+        break;
+      case 'symbols':
+        this.options.useSymbols = !this.options.useSymbols;
+        break;
+    }
+  }
+  onChangeLength(value: string): void {
+    const parsedValue = parseInt(value, 10);
+    if (!isNaN(parsedValue)) {
+      this.options.length = parsedValue;
+    }
+  }
+  onButtonClick(): void {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+    let generatedPassword = '';
+    if (this.options.useLetters) {
+      validChars += letters;
+    }
+    if (this.options.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.options.useSymbols) {
+      validChars += symbols;
+    }
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.options.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
